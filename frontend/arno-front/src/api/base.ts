3c5d0fbf65a71dd95a27f4dd120b166b74812ebb
savaw <savaw97@gmail.com>
2022-07-16 13:21:06 +0430

    Add BaseAPI class

--- frontend/arno-front/src/api/base.ts
+++ frontend/arno-front/src/api/base.ts
@@ -32,3 +32,68 @@ const sendGetRequest = async (url: string) => {
   }
 };
 export { sendGetRequest };
+
+
+interface APIRequest {
+  path: string
+  body: object|null
+  header: object|null
+}
+
+
+interface APIResponse{
+  success: boolean
+  data: object|null
+  error: string
+}
+
+abstract class BaseAPI {
+  base_url: string = BASE_URL
+  base_path: string
+
+  constructor(base_url: string, base_path: string) {
+    this.base_url = base_url;
+    this.base_path = base_path
+  }
+  
+  async sendGetRequest(r: APIRequest) : Promise<APIResponse> {
+    
+      try {
+        const config = {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            ...r.header
+          },
+        };
+    
+        const url = `${this.base_url}${this.base_path}${r.path}`
+        const res = await axios.get(url, config);
+
+        const { data } = res;
+        return { 
+          success: true, 
+          data: data ,
+          error: ""
+        };
+      } catch (error: any) {
+        if (error.code === "ERR_NETWORK") {
+          return {
+            success: false,
+            data: null,
+            error: "در ارتباط با سرور خطایی رخ داد. مجددا تلاش کنید..",
+          }
+        } else {
+          return {
+            data: null,
+            success: false,
+            error: error.response.data,
+          };
+        }
+      }
+  }
+  async sendAuthorizedGetRequest(r: APIRequest) : Promise<APIResponse> {
+      const token = window.localStorage.getItem("token");
+      r.header = {...r.header, Authorization: `Token ${token}`}
+      return this.sendGetRequest(r)
+  }
+}
