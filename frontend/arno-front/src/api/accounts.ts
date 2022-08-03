beb1ee4d1f424a6c0a18ecea464c79c20f980986
AlirezaT99 <tajmirriahia@yahoo.com>
2022-08-03 18:15:39 +0430

    Add function for most account API calls

--- frontend/arno-front/src/api/accounts.ts
+++ frontend/arno-front/src/api/accounts.ts
@@ -1,4 +1,4 @@
-import { BaseAPI, BaseListAPI } from "./base";
+import { BaseListAPI } from "./base";
 
 export class AccountAPI extends BaseListAPI {
   protected static instance: AccountAPI;
@@ -24,6 +24,78 @@ export class AccountAPI extends BaseListAPI {
     });
 
     console.info("getMyAccount", response);
+    return response;
+  }
+
+  async editMyProfile(newInfo: any) {
+    const response = await this.sendAuthorizedPutRequest({
+      path: `edit/`,
+      body: newInfo,
+      headers: null,
+      params: null,
+    });
+
+    console.info("editMyProfile", response);
+    return response;
+  }
+
+  async getSpecialities() {
+    const response = await this.sendAuthorizedGetRequest({
+      path: "speciality/",
+      body: null,
+      headers: null,
+      params: null,
+    });
+
+    console.info("getSpecialities", response);
+    return response;
+  }
+
+  async defineNewSpeciality(specialty: any) {
+    const response = await this.sendAuthorizedPostRequest({
+      path: `speciality/`,
+      body: specialty,
+      headers: null,
+      params: null,
+    });
+
+    console.info("defineNewSpeciality", response);
+    return response;
+  }
+
+  async addSpeciality(specialityId: number) {
+    const response = await this.sendAuthorizedPostRequest({
+      path: "speciality/add/",
+      body: { speciality_id: specialityId },
+      headers: null,
+      params: null,
+    });
+
+    console.info("addSpeciality", response);
+    return response;
+  }
+
+  async removeSpeciality(specialityId: number) {
+    const response = await this.sendAuthorizedDeleteRequest({
+      path: "speciality/add/",
+      body: { speciality_id: specialityId },
+      headers: null,
+      params: null,
+    });
+
+    console.info("removeSpeciality", response);
+    return response;
+  }
+
+  async confirmSpecialist(specialistId: number) {
+    const response = await this.sendAuthorizedPostRequest({
+      path: "specialist/confirm/",
+      body: { specialist_id: specialistId },
+      headers: null,
+      params: null,
+    });
+
+    console.info("confirmSpecialist", response);
     return response;
   }
 }
