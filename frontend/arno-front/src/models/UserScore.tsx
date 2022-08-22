6368256124cd99359ab325bea7f4beb5cb4e9434
AlirezaT99 <tajmirriahia@yahoo.com>
2022-08-22 18:23:10 +0430

    Minor visual improvements

--- frontend/arno-front/src/models/UserScore.tsx
+++ frontend/arno-front/src/models/UserScore.tsx
@@ -6,7 +6,7 @@ import { RatingFillColorArray } from "../assets/consts";
 export const UserScore = (props: { score: number }) => {
   const color = RatingFillColorArray[Math.floor(Math.min(99, props.score) / 10)];
   return <Group align="center" spacing={2}>
+    {"" + props.score / 20}
     <Star color={color} strokeWidth={2.5} size={20} fill={color}/>
-    {props.score / 20}
   </Group>;
 };
